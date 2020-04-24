5630043a746ba6fa5e80d7b8f5a07ed47946c51b
Affan100 <affan_patan@hotmail.com>
2020-04-24 23:29:57 +0700

    Editcommit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyABkAvfQINIRpudOhg3Znj2UA76Ff_RIv0",
+    apiKey: "****",
     authDomain: "mini-project-2755f.firebaseapp.com",
     databaseURL: "https://mini-project-2755f.firebaseio.com",
     projectId: "mini-project-2755f",
     storageBucket: "mini-project-2755f.appspot.com",
-    messagingSenderId: "983111367936",
-    appId: "1:983111367936:web:dff3ba066dc60c57c907e5",
-    measurementId: "G-0MT2SM746G"
+    messagingSenderId: "******",
+    appId: "******",
+    measurementId: "*****"
 };
 
 const fb = firebase.initializeApp(config)
